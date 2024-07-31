9b4ecf9dd1f6cf8ab70f6dddd90b07fe2dddd145
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2024-07-31 09:56:47 +0700

    add check version

---
+++ app/Services/OCRService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class OCRService
+{
+    public function recognizeText($imagePath)
+    {
+        $apiKey = env('OCR_SPACE_API_KEY');
+        $response = Http::attach(
+            'file',
+            file_get_contents($imagePath),
+            basename($imagePath)
+        )->post('https://api.ocr.space/parse/image', [
+            'apikey' => $apiKey,
+            'language' => 'eng',
+            'isTable' => 'true', // Menambahkan parameter isTable
+            'OCREngine' => 2,  // Menambahkan parameter OCREngine
+        ]);
+
+        $data = $response->json();
+        if ($data['IsErroredOnProcessing']) {
+            throw new \Exception($data['ErrorMessage'][0]);
+        }
+
+        return ['text' => $data['ParsedResults'][0]['ParsedText']];
+    }
+}
